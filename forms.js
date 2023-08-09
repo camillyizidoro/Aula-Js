8d0cf62cb2ccf48dcfc599b97095274fa109e6ed
Camilly Izidoro <134517571+camillyizidoro@users.noreply.github.com>
2023-08-09 10:33:41 -0300

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,19 @@
+function Enviar (){
+    var nome= document.getElementById("nomeid");
+    
+    if(nome.value !=""){
+        alert("Obrigado novinho(a)" + nome.value + "Seu formulário foi enviado e descartado")
+    }
+    else{
+        alert("what is this")
+    }
+
+    var nome= document.getElementById("foneid");
+    
+    if(nome.value !=""){
+        alert("Obrigado novinho(a)" + nome.value + "Seu formulário foi enviado e descartado")
+    }
+    else{
+        alert("what is this")
+    }
+}
